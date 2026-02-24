d9e32aaca450def607639b364c0b0ee48ea5df52
jimschaeffer <130175038+jimschaeffer@users.noreply.github.com>
2026-02-24 09:17:56 -0600

    rx homepage

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,22 +1,22 @@
 module.exports = {
-    name: "Precision RX Compounding Pharmacy",
-    email: "help@codestitch.app",
-    phoneForTel: "+12058230780",
-    phoneFormatted: "(205) 823-0780",
-    address: {
-        lineOne: "3325 Rocky Ridge Plz, Ste 207",
-        city: "Vestavia Hills",
-        state: "AL",
-        zip: "35243",
-        country: "US",
-        mapLink: "https://maps.app.goo.gl/CfnQvxVxwqKFbkVv7",
-    },
-    socials: {
-        facebook: "https://www.facebook.com/p/Precision-Rx-61566822557757/",
-        instagram: "https://www.instagram.com/precisionrx_/",
-    },
-    //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.precisionrxcompounding.com",
-    // Passing the isProduction variable for use in HTML templates
-    isProduction: process.env.ELEVENTY_ENV === "PROD",
+  name: "Precision RX Compounding Pharmacy",
+  email: "info@precisionrxcompounding.com",
+  phoneForTel: "+12058230780",
+  phoneFormatted: "(205) 823-0780",
+  address: {
+    lineOne: "3325 Rocky Ridge Plz, Ste 207",
+    city: "Vestavia Hills",
+    state: "AL",
+    zip: "35243",
+    country: "US",
+    mapLink: "https://maps.app.goo.gl/CfnQvxVxwqKFbkVv7",
+  },
+  socials: {
+    facebook: "https://www.facebook.com/p/Precision-Rx-61566822557757/",
+    instagram: "https://www.instagram.com/precisionrx_/",
+  },
+  //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
+  domain: "https://www.precisionrxcompounding.com",
+  // Passing the isProduction variable for use in HTML templates
+  isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
